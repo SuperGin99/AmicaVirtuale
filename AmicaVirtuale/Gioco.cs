92041894512f5f6b9f5bd9dc725c481e045aa739
Ginevra Giorgi <ginevra.giorgi15@gmail.com>
2025-07-14 18:39:58 +0200

    primo invio

---
+++ AmicaVirtuale/Gioco.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmicaVirtuale
+{
+    class Gioco
+    {
+        public string nome { get; set; }
+        private int punteggioApp = 0;
+        private int punteggioUtente = 0;
+        public Gioco(string nomeGiocatore)
+        {
+            // Inizializza il gioco
+            nome = nomeGiocatore;
+            Console.WriteLine($"Iniziamo a giocare {nome}!");
+        }
+
+        public void IndovinaNumero()
+        {
+            string input="si";
+            Random random = new Random();
+
+            while (input.ToLower() == "si")
+            {
+                Console.WriteLine("Ottimo! Pensa a un numero tra 1 e 10, e io proverò a indovinarlo. Scrivilo e io non guardo ;)");
+                int numeroInserito;
+                int.TryParse(Console.ReadLine(), out numeroInserito);
+
+                if (numeroInserito < 1 || numeroInserito > 10)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Il numero deve essere tra 1 e 10, riprova!");
+                    // Sequenza di suoni per input errato
+                    Console.Beep(800, 200);
+                    Console.Beep(600, 200);
+                    Console.Beep(400, 300);
+                    Console.Beep(300, 500);
+
+                    Console.ResetColor();
+                    continue;
+                }
+
+                int numeroPensato = random.Next(1, 10);
+                Console.WriteLine($"Io ho pensato al numero: {numeroPensato}");
+
+                if (numeroPensato == numeroInserito)
+                {
+                    Console.WriteLine("Ho indovinato il tuo numero! Vuoi giocare ancora?");
+                    punteggioApp++;
+                }
+                else
+                {
+                    Console.WriteLine($"Non ho indovinato {nome}, il tuo numero era {numeroInserito}. Vuoi giocare ancora?");
+                    punteggioUtente++;
+                }
+                input = Console.ReadLine();
+            }
+            if (punteggioApp != 0 && punteggioUtente != 0)
+            {
+                Console.WriteLine($"Hai vinto {punteggioUtente} volte e io {punteggioApp} volte. È stato divertente giocare con te, {nome}!");
+            }
+
+            Console.WriteLine("Va bene, magari un'altra volta!");
+        }
+
+    }
+}
---
+++ AmicaVirtuale/Program.cs
@@ -0,0 +1,61 @@
+﻿
+using AmicaVirtuale;
+using System;
+
+string input;
+string nome;
+int punteggioApp = 0;
+int punteggioUtente = 0;
+
+
+Console.WriteLine("Ciao io sono la tua nuova amica virtuale! mi chiamo SuperG r/n E tu come ti chiami?");
+nome = Console.ReadLine();
+Console.WriteLine($"Ciao {nome}, come stai?");
+input = Console.ReadLine();
+
+switch (input.ToLower())
+{
+    case "bene":
+        Console.WriteLine($"Mi fa piacere {nome} che tu stia bene!");
+        break;
+    case "male":
+        Console.WriteLine($"Mi dispiace {nome}, spero che tu ti senta meglio presto.");
+        break;
+    case "cosi e cosi":
+        Console.WriteLine($"Capisco {nome}, mi dispiace, ci sono giorni così.");
+        break;
+    default:
+        Console.WriteLine("Grazie per aver condiviso come ti senti.");
+        break;
+}
+
+
+Console.WriteLine($"A cosa ti va di giocare oggi, {nome}?");
+Console.WriteLine("1. Indovina il numero");
+Console.WriteLine("2. indovinelli");
+Console.WriteLine("3. il gioco dell'eco");
+
+input = Console.ReadLine();
+
+switch (input.ToLower())
+{
+    case "1":
+        Gioco gioco = new Gioco(nome);
+        gioco.IndovinaNumero();
+        break;
+    case "2":
+        
+        break;
+    case "3":
+        
+        break;
+    default:
+        Console.WriteLine("Devi inserire un numero.");
+        break;
+}
+
+
+
+
+
+
